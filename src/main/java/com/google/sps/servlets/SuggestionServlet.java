c0f32529585d9e8534c1202fd597a3ef79de96d9
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-07-28 16:27:30 -0700

    Adds servlet stuff for suggestion on tutorials page, not tested

---
+++ src/main/java/com/google/sps/servlets/SuggestionServlet.java
@@ -0,0 +1,32 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet responsible for creating a suggestion. */
+@WebServlet("/suggestion")
+public class SuggestionServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String platform = request.getParameter("platform");
+    String addition = request.getParameter("addition");
+    long timestamp = System.currentTimeMillis();
+
+    Entity suggestionEntity = new Entity("Suggestion");
+    suggestionEntity.setProperty("platform", platform);
+    suggestionEntity.setProperty("addition", addition);
+    suggestionEntity.setProperty("timestamp", timestamp);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(suggestionEntity);
+
+    response.sendRedirect("/tutorials.html");
+  }
+}
