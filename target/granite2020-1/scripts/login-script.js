b00d8c77ad2c15abf6e09e2eef293fd6eb833cd2
Praneet <praneetmek@gmail.com>
2020-07-29 09:32:07 -0400

    nav bar moved to separate file for use on multiple pages

---
+++ target/granite2020-1/scripts/login-script.js
@@ -0,0 +1,8 @@
+$("#nav-placeholder").load("../nav.html")
+
+fetch("/account").then(response => response.json()).then(loginlink => {
+        console.log("setting link to " + loginlink) 
+
+        $("#login").attr("href",loginlink)
+    }
+)
