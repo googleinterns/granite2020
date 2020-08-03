7c4ae70116246dbe59570f987842e215b0cb6b58
Praneet <praneetmek@gmail.com>
2020-08-03 13:18:28 -0400

    fixing java styling issues

--- src/main/java/com/google/sps/servlets/AccountData.java
+++ src/main/java/com/google/sps/servlets/AccountData.java
@@ -5,7 +5,6 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.Gson;
@@ -24,6 +23,7 @@ public class AccountData extends HttpServlet {
     private final boolean loggedIn;
     private final String logInOutURL;
     private final String signUpURL;
+
     public JsonHelper(Boolean loggedIn, String logInOutURL, String signUpURL) {
       this.loggedIn = loggedIn;
       this.logInOutURL = logInOutURL;
