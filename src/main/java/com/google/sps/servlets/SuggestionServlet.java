12cc68412db51cf55517ea7993b90be8d8b4ed17
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-11 16:21:10 -0700

    linter issues

--- src/main/java/com/google/sps/servlets/SuggestionServlet.java
+++ src/main/java/com/google/sps/servlets/SuggestionServlet.java
@@ -16,9 +16,7 @@ public class SuggestionServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String platform = request.getParameter("platform");
-    System.out.println("Platform: " + platform );
     String addition = request.getParameter("addition");
-    System.out.println("Addition: " + addition );
     long timestamp = System.currentTimeMillis();
 
     Entity suggestionEntity = new Entity("Suggestion");
