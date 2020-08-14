57daf39cba9fbb61dfbb93a16ed1542ba7182119
Rae <rmr167@gmail.com>
2020-08-13 17:24:55 -0700

    Add styling and mustache rendering and sorting and filtering features

--- src/main/java/com/google/sps/data/ForumElement.java
+++ src/main/java/com/google/sps/data/ForumElement.java
@@ -10,22 +10,25 @@ public final class ForumElement {
   public static final String TEXT_PROPERTY = "text";
   public static final String TIMESTAMP_PROPERTY = "timestamp";
   public static final String LIKES_PROPERTY = "likes";
+  public static final String NUMBER_REPLIES_PROPERTY = "numberReplies";
   private final long id;
   private final long parentId;
   private final String topic;
   private final long timestamp;
   private final long likes;
   private final String text;
+  private final long numberReplies;
 
   private ForumElement(
-      long id, long parentId, String topic, long timestamp, long likes, String text) {
+      long id, long parentId, String topic, long timestamp, long likes, String text, long numberReplies) {
     this.id = id;
     this.parentId = parentId;
     this.topic = topic;
     this.timestamp = timestamp;
     this.likes = likes;
     this.text = text;
-    // TODO: Add user data to comment data
+    this.numberReplies = numberReplies;
+    // TODO: Add user data to comment data and accepted answer
   }
 
   /** Inputs an entity and returns a new forum element using the fields of the entity */
@@ -36,8 +39,9 @@ public final class ForumElement {
     long timestamp = (long) entity.getProperty(TIMESTAMP_PROPERTY);
     long likes = (long) entity.getProperty(LIKES_PROPERTY);
     String text = (String) entity.getProperty(TEXT_PROPERTY);
+    long numberReplies = (long) entity.getProperty(NUMBER_REPLIES_PROPERTY);
     // TODO: Get user information
 
-    return new ForumElement(id, parentId, topic, timestamp, likes, text);
+    return new ForumElement(id, parentId, topic, timestamp, likes, text, numberReplies);
   }
 }
--- src/main/java/com/google/sps/servlets/ForumServlet.java
+++ src/main/java/com/google/sps/servlets/ForumServlet.java
@@ -7,13 +7,17 @@ import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
 import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.gson.Gson;
 import com.google.sps.data.ForumElement;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,6 +27,14 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that handles forum data (questions, comments, and replies) */
 @WebServlet("/forum")
 public class ForumServlet extends HttpServlet {
+  public static final String ACTION_PROPERTY = "action";
+  public static final String REPLY_PROPERTY = "reply";
+  public static final String FILTER_PROPERTY = "filter";
+  public static final String SORT_PROPERTY = "sort";
+
+  /* Saved fields of the filters for questions */
+  private String topicFilter = "all";
+  private String sortFilter = ForumElement.TIMESTAMP_PROPERTY;
 
   /** Get request to the Datastore and responds with the forum elements with the id input */
   @Override
@@ -39,30 +51,49 @@ public class ForumServlet extends HttpServlet {
    */
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    long id = Long.parseLong(request.getParameter(ForumElement.ID_PROPERTY));
-    String likes = request.getParameter(ForumElement.LIKES_PROPERTY);
+    String action = request.getParameter(ACTION_PROPERTY);
 
-    if (likes.equals("true")) {
+    if (action.equals(ForumElement.LIKES_PROPERTY)) {
       /* Increments likes of element */
-      incrementLikes(id);
-      response.sendRedirect("/forum.html");
-    } else {
+      long id = Long.parseLong(request.getParameter(ForumElement.ID_PROPERTY));
+      incrementProperty(ForumElement.LIKES_PROPERTY, id);
+    } 
+    
+    if (action.equals(REPLY_PROPERTY)) {
       /* Adds new forum element */
+      long id = Long.parseLong(request.getParameter(ForumElement.ID_PROPERTY));
+
+      /* Obtains the topic if the element is a question with no parentId */
       String topic = "none";
       if (id == -1) {
         topic = request.getParameter(ForumElement.TOPIC_PROPERTY);
       }
+
       String text = request.getParameter(ForumElement.TEXT_PROPERTY);
       addForumEntity(id, topic, text);
+
+      /* Increments the replies of the parent if there is a parent */
+      if (id != -1) {
+        incrementProperty(ForumElement.NUMBER_REPLIES_PROPERTY, id);
+      }
+
       response.sendRedirect("/forum.html");
+    }
+
+    if (action.equals(FILTER_PROPERTY)) {
+      /* Gets the new filter preferences */
+      String topic = request.getParameter(ForumElement.TOPIC_PROPERTY);
+      String sort = request.getParameter(SORT_PROPERTY);
+      topicFilter = topic;
+      sortFilter = sort;
+      response.sendRedirect("/forum.html?topic=" + topicFilter + "&sort=" + sortFilter);
     }
   }
 
   /** Gets a list of forum elements from Datastore with id given */
   private List<ForumElement> getForumElements(long id) {
-    Filter filter = new FilterPredicate(ForumElement.PARENT_ID_PROPERTY, FilterOperator.EQUAL, id);
-    Query query = new Query("ForumElement").setFilter(filter);
-    // TODO: Add sort of likes descending
+    Query query = getQuery(id);
+    
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
 
@@ -73,8 +104,31 @@ public class ForumServlet extends HttpServlet {
     return elements;
   }
 
-  /** Increments likes of element given id in Datastore */
-  private void incrementLikes(long id) {
+  /* Returns the query for the Datastore given the id and topic filters and a given sorting */
+  private Query getQuery(long id) {
+    Filter filter;
+    String sort;
+    
+    /* Filter by topic if there is a topic filter and the filter is for questions */
+    if (topicFilter.equals("all") || id != -1) {
+      filter = new FilterPredicate(ForumElement.PARENT_ID_PROPERTY, FilterOperator.EQUAL, id);
+    } else {
+      filter = new CompositeFilter(CompositeFilterOperator.AND, Arrays.asList(new FilterPredicate(ForumElement.PARENT_ID_PROPERTY, FilterOperator.EQUAL, id), new FilterPredicate(ForumElement.TOPIC_PROPERTY, FilterOperator.EQUAL, topicFilter)));
+    }
+
+    /* Determine sort property - if question use given sort property, if comment always sort by likes */
+    if (id == -1) {
+      sort = sortFilter;
+    } else {
+      sort = ForumElement.LIKES_PROPERTY;
+    }
+
+    Query query = new Query("ForumElement").setFilter(filter).addSort(sort, SortDirection.DESCENDING);
+    return query;
+  }
+
+  /** Increments property of element given id in Datastore - used for likes and replies */
+  private void incrementProperty(String property, long id) {
     Key key = KeyFactory.createKey("ForumElement", id);
     Filter filter = new FilterPredicate(Entity.KEY_RESERVED_PROPERTY, FilterOperator.EQUAL, key);
     Query query = new Query("ForumElement").setFilter(filter);
@@ -82,8 +136,8 @@ public class ForumServlet extends HttpServlet {
     PreparedQuery results = datastore.prepare(query);
 
     for (Entity entity : results.asIterable()) {
-      long likes = (long) entity.getProperty(ForumElement.LIKES_PROPERTY);
-      entity.setProperty(ForumElement.LIKES_PROPERTY, likes + 1);
+      long num = (long) entity.getProperty(property);
+      entity.setProperty(property, num + 1);
       datastore.put(entity);
     }
   }
@@ -97,6 +151,7 @@ public class ForumServlet extends HttpServlet {
     forumEntity.setProperty(ForumElement.TIMESTAMP_PROPERTY, timestamp);
     forumEntity.setProperty(ForumElement.LIKES_PROPERTY, 0);
     forumEntity.setProperty(ForumElement.TEXT_PROPERTY, text);
+    forumEntity.setProperty(ForumElement.NUMBER_REPLIES_PROPERTY, 0);
     // TODO: Include User data
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
