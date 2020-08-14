3e53757720f6c7cda0cdf42689f0565d02267ee5
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-14 16:43:54 -0700

    Responsive web design, consistent buttons

--- src/main/webapp/scripts/suggest-script.js
+++ src/main/webapp/scripts/suggest-script.js
@@ -1,1 +1,26 @@
+/*
+
+function makeSuggestion(idhandler) {
+
+}
+
 //$.post('/forum?id=' + id.toString() + '&likes=true');
+
+/**
+ *  Posts to server to increment likes for a element
+ *
+ *  @param {S.Event} idHandler onclick handler that contains the id data
+function incrementLikes(idHandler) {
+  const id = idHandler.data;
+  const elementId = 'element-' + id.toString();
+  $.post('/forum?id=' + id.toString() + '&action=likes');
+  const likes = parseInt($('#' + elementId + ' .likes').text());
+  $('#' + elementId + ' .likes').text(likes + 1);
+}
+
+
+this.platform = platform;
+    this.addition = addition;
+    this.timestamp = timestamp;
+
+    */
