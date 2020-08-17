79731fc9a397175a0d7d11b67532d57831dd474b
Praneet <praneetmek@gmail.com>
2020-08-17 08:38:25 -0400

    js lint fix

--- src/main/webapp/scripts/index-script.js
+++ src/main/webapp/scripts/index-script.js
@@ -1,10 +1,10 @@
-import {auth2, signIn, initPromise} from "./account-info.js"
-import {updateBar} from "./nav-bar.js"
-initPromise.then(function(){
-    updatePage();
+import {auth2, signIn, initPromise} from './account-info.js';
+import {updateBar} from './nav-bar.js';
+initPromise.then(function() {
+  updatePage();
 
-    $(".sign-in").click(function(){
-      onSignIn();
+  $(".sign-in").click(function(){
+    onSignIn();
   })
   auth2.isSignedIn.listen(updatePage);
 
@@ -12,19 +12,18 @@ initPromise.then(function(){
 
 
 export function onSignIn(){
-    return signIn().then(function(){
-        updatePage();
-        updateBar();
-    })
-  }
+  return signIn().then(function(){
+    updatePage();
+    updateBar();
+  })
+}
 
 
 function updatePage(){
-    if(auth2.isSignedIn.get()){
-        $("#account-functions").css("display", "none")
-    }
-    else{
-        $("#account-functions").css("display", "block")
-    }
+  if(auth2.isSignedIn.get()){
+    $("#account-functions").css("display", "none")
+  } else {
+    $("#account-functions").css("display", "block")
+  }
 }
 
