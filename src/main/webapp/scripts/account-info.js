6bee9fd18a003b446c473a297e2165440b169a78
Praneet <praneetmek@gmail.com>
2020-08-17 08:36:14 -0400

    js lint fix

--- src/main/webapp/scripts/account-info.js
+++ src/main/webapp/scripts/account-info.js
@@ -4,21 +4,20 @@ const initPromise = init();
 
 /**
  * Initializes the auth2 variable
- * @return Promise which resolves when auth2 is initalized
+ * @return {Promise} Promise which resolves when auth2 is initalized
  */
 function init() {
   return new Promise(function(resolve, reject) {
     gapi.load('auth2', function() {
       auth2 = gapi.auth2.init();
       resolve();
-    });
-  });
+    });});
 
 }
 
 /**
  * Signs user in and sets profile variable
- * @return Promise which resolves when profile is set
+ * @return {Promise} Promise which resolves when profile is set
  */
 function signIn() {
   return auth2.signIn().then(function() {
@@ -28,7 +27,8 @@ function signIn() {
 
 /**
  * Signs out user and clear profile variable
- * @return Promise which resolv es when user signs out and profile is set to null
+ * @return {Promise} Promise which resolves when user 
+ * signs out and profile is set to null
  */
 function signOut() {
   return auth2.signOut().then(function() {
