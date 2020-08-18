ba8693f9a3a4ace7315691c34fa04d2d690df97b
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-18 16:25:51 -0700

    Organized button vs input type submit vs account functions, added onclick to javascript

--- src/main/webapp/scripts/suggest-script.js
+++ src/main/webapp/scripts/suggest-script.js
@@ -1,8 +1,16 @@
-/* eslint-disable */
+/** 
+ * Gets executed when button is clicked
+ */
+window.onload = function(){
+  document.getElementById("make-suggestion").onclick = function() {makeSuggestion()};
+};
+
 /**
  * Posts to server the suggestion made by the user
+ * 
+ * Ignoring linter otherwise makeSuggestion is unsued but it is called in tutorials.html
  */
- function makeSuggestion() {
+function makeSuggestion() {
   const platElem = document.getElementById('platform').value;
   const addElem = document.getElementById('addition').value;
   $.post('/new-suggestion?platform=' + platElem + '&addition=' + addElem);
