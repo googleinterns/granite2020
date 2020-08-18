9769abffeca29f4f437ee165eb6495315579cd5e
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-18 12:41:57 -0700

    Add more tutorial content, standardize styling, suggestion no refresh on submission

--- src/main/webapp/scripts/suggest-script.js
+++ src/main/webapp/scripts/suggest-script.js
@@ -1,7 +1,8 @@
+/* eslint-disable */
 /**
  * Posts to server the suggestion made by the user
  */
-function makeSuggestion() {
+ function makeSuggestion() {
   const platElem = document.getElementById('platform').value;
   const addElem = document.getElementById('addition').value;
   $.post('/new-suggestion?platform=' + platElem + '&addition=' + addElem);
