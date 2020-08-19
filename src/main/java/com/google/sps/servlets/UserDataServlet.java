ea3d42eecf229693383899a98c33d493460d0123
Praneet <praneetmek@gmail.com>
2020-08-19 11:52:30 -0400

    sent user data to back end

--- src/main/java/com/google/sps/servlets/UserDataServlet.java
+++ src/main/java/com/google/sps/servlets/UserDataServlet.java
@@ -13,6 +13,7 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 import com.google.gson.Gson;
 import com.google.sps.data.ForumElement;
 import java.io.*;
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
@@ -22,25 +23,103 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.json.gson.GsonFactory;
 
 /** Servlet that handles forum data (questions, comments, and replies) */
 @WebServlet("/account")
 public class UserDataServlet extends HttpServlet {
+  private static final String ID_PROPERTY = "id";
+  private static final String NAME_PROPERTY = "name";
+  private static final String EMAIL_PROPERTY = "email";
+  private static final String LIKED_PROPERTY = "liked";
+  private static final String COMMENT_PROPERTY = "comments";
 
-  /** Get request to the Datastore and responds with the forum elements with the id input */
+  private static final String DATASTORE_USER = "User";
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
   }
 
-  /**
-   * Post request to the Datastore that posts a new element or increments the likes of an element
-   */
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String id = request.getParameter("idtoken");
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException  {
+    String idTokenString = request.getParameter("idtoken");
+    UrlFetchTransport transport = new UrlFetchTransport();
+
+    GsonFactory gsonFactory = new GsonFactory();
+
+
+
+    GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(UrlFetchTransport.getDefaultInstance(), gsonFactory)
+    // Specify the CLIENT_ID of the app that accesses the backend:
+    .setAudience(Collections.singletonList("757099697912-i6jll98mfgochdo2vgjcovf64pepjesc.apps.googleusercontent.com"))
+    // Or, if multiple clients access the backend:
+    //.setAudience(Arrays.asList(CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3))
+    .build();
+
+    GoogleIdToken idToken;
+    try{
+      idToken = verifier.verify(idTokenString);
+    } catch(Exception e) {
+      idToken= null;
+    }
+    if (idToken != null) {
+      Payload payload = idToken.getPayload();
+
+      String userId = payload.getSubject();
+      String email = payload.getEmail();
+      String name = (String) payload.get("name");
+      String pictureUrl = (String) payload.get("picture");
+      String locale = (String) payload.get("locale");
+      String familyName = (String) payload.get("family_name");
+      String givenName = (String) payload.get("given_name");
+
+      Entity user = getUserEntity(userId);
+      if(user == null){
+        addUser(userId, name, email);
+        System.out.println("user added!");
+      }
+
+    } else {
+      System.out.println("Invalid ID token.");
+    }
+
   }
 
+  private Entity getUserEntity(String id){
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query =
+        new Query("User")
+            .setFilter(new Query.FilterPredicate(ID_PROPERTY, Query.FilterOperator.EQUAL, id));
+    PreparedQuery results = datastore.prepare(query);
+
+    Entity entity = results.asSingleEntity();
+
+    return entity;
+  }
+
+  private void addUser(String id, String name, String email){
+    Entity userEntity = new Entity(DATASTORE_USER);
+
+    userEntity.setProperty(ID_PROPERTY, id);
+    userEntity.setProperty(NAME_PROPERTY, name);
+    userEntity.setProperty(EMAIL_PROPERTY, email);
+    userEntity.setProperty(LIKED_PROPERTY, convertToJson(new ArrayList<String>()));
+    userEntity.setProperty(COMMENT_PROPERTY, convertToJson(new ArrayList<String>()));
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(userEntity);
+
+  }
+
+  private String convertToJson(ArrayList<String> arrayList){
+    Gson gson = new Gson();
+    String json = gson.toJson(arrayList);
+    return json;
+
+  }
 
 
 
