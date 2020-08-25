67d0965bbf9e7ceed2ec6a896152df39b087604b
Rae Moar <rmr167@gmail.com>
2020-08-25 16:52:21 -0700

    linter style errors

--- src/main/webapp/scripts/forum-script.js
+++ src/main/webapp/scripts/forum-script.js
@@ -361,19 +361,6 @@ function acceptComment(idHandler) {
 }
 
 /**
- *  Posts to server to increment likes for a element
- *
- *  @param {S.Event} idHandler onclick handler that contains the id data
- */
-function incrementLikes(idHandler) {
-  const id = idHandler.data;
-  const elementId = 'element-' + id.toString();
-  $.post('/forum?id=' + id.toString() + '&action=likes');
-  const likes = parseInt($('#' + elementId + ' .likes').text());
-  $('#' + elementId + ' .likes').text(likes + 1);
-}
-
-/**
  *  Displays the reply form for a given element
  *
  *  @param {S.Event} idHandler onclick handler that contains the id data
