23410f5db1231fe2da77457877cd45bf76f55e0b
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-25 11:24:30 -0700

    backticks and indentation

--- src/main/webapp/scripts/tutorial-scripts/tutorial-template.js
+++ src/main/webapp/scripts/tutorial-scripts/tutorial-template.js
@@ -3,33 +3,34 @@
 
 const tutorialTemplate =
 '<div id="header">' +
-'<h1>{{toolName}}</h1>' +
-'<h2>{{toolDescription}}</h2>' +
-`<a href="{{toolLink}}" target="_blank" rel="noopener"
->{{toolName}} Link</a>` +
+  '<h1>{{toolName}}</h1>' +
+  '<h2>{{toolDescription}}</h2>' +
+  `<a href="{{toolLink}}" target="_blank" rel="noopener"
+  >{{toolName}} Link</a>` +
 '</div>' +
+
 '<div id="content">' +
-'<div id="content-left">' +
-'<h1>Useful Links</h1>' +
-'{{#usefulLinks}}' +
-'<h3>{{linkTitle}}</h3>'+
-'{{#links}}' +
-'<li>' +
-`<a href="{{url}}" target="_blank" rel="noopener"
->{{linkName}}</a>` +
-'<li>' +
-'{{/links}}' +
-'{{/usefulLinks}}' +
-'</div>' +
-'<div id="content-right">' +
-'<h1>Useful Videos</h1>' +
-'{{#usefulVids}}' +
-'<h2>{{linkTitle}}</h2>' +
-`<iframe class="tutorials-vid" src="{{vidLink}}"
-allow="accelerometer; autoplay;
-encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` +
-'{{/usefulVids}}' +
-'</div>' +
+  '<div id="content-left">' +
+    '<h1>Useful Links</h1>' +
+    '{{#usefulLinks}}' +
+      '<h3>{{linkTitle}}</h3>'+
+      '{{#links}}' +
+      '<li>' +
+        `<a href="{{url}}" target="_blank" rel="noopener"
+        >{{linkName}}</a>` +
+      '<li>' +
+      '{{/links}}' +
+    '{{/usefulLinks}}' +
+  '</div>' +
+  '<div id="content-right">' +
+    '<h1>Useful Videos</h1>' +
+    '{{#usefulVids}}' +
+      '<h2>{{linkTitle}}</h2>' +
+      `<iframe class="tutorials-vid" src="{{vidLink}}"
+      allow="accelerometer; autoplay;
+      encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` +
+    '{{/usefulVids}}' +
+  '</div>' +
 '</div>';
 
 export {tutorialTemplate};
