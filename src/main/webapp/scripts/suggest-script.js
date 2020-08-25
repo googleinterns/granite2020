97c2fa2d77731907c108548ef2b7e033a56301e7
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-25 11:11:13 -0700

    trailing whitespace

--- src/main/webapp/scripts/suggest-script.js
+++ src/main/webapp/scripts/suggest-script.js
@@ -14,7 +14,7 @@ window.onload = function() {
 function makeSuggestion() {
   const platformElement = document.getElementById('platform');
   const additionElement = document.getElementById('addition');
-  $.post('/new-suggestion?platform=' + platformElement.value + 
+  $.post('/new-suggestion?platform=' + platformElement.value +
     '&addition=' + additionElement.value);
   platformElement.value = '';
   additionElement.value = '';
