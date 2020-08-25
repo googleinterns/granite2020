b9da211c71cd90dd30f328266b5e9c5d0ca4f7b0
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-24 17:48:41 -0700

    Linter errors

--- src/main/webapp/scripts/tutorial-scripts/zoom-template.js
+++ src/main/webapp/scripts/tutorial-scripts/zoom-template.js
@@ -11,9 +11,9 @@ const zoomData = {
           linkName: 'Zoom Video Tutorials',
         },
         {url: 'https://support.zoom.us/hc/en-us/categories/200101697',
-        linkName: 'Zoom getting started',
+          linkName: 'Zoom getting started',
         },
-      ]
+      ],
     },
 
     {linkTitle: 'Desktop',
@@ -21,43 +21,43 @@ const zoomData = {
         {url: 'https://support.zoom.us/hc/en-us/articles/201362033-Getting-Started-on-Windows-and-Mac',
           linkName: 'Getting started on Windows and Mac',
         },
-      ]
+      ],
     },
 
     {linkTitle: 'Mobile',
       links: [
         {url: 'https://support.zoom.us/hc/en-us/articles/201362993-Getting-Started-with-iOS',
-          linkName: 'Getting started with IOS (Apple)'
+          linkName: 'Getting started with IOS (Apple)',
         },
         {url: 'https://support.zoom.us/hc/en-us/articles/200942759-Getting-Started-with-Android',
           linkName: 'Getting started with Android (Google)',
         },
-      ]
+      ],
     },
 
     {linkTitle: 'Zoom General Help',
       links: [
         {url: 'https://support.zoom.us/hc/en-us/articles/201362193-Joining-a-meeting',
-          linkName: 'Joining a meeting'
+          linkName: 'Joining a meeting',
         },
         {url: 'https://support.zoom.us/hc/en-us/articles/201362613-How-Do-I-Host-A-Video-Meeting-',
-          linkName: 'Hosting a meeting'
+          linkName: 'Hosting a meeting',
         },
         {url: 'https://support.zoom.us/hc/en-us/articles/201362413-Scheduling-meetings',
-          linkName: 'Scheduling a meeting'
+          linkName: 'Scheduling a meeting',
         },
-      ]
+      ],
     },
 
     {linkTitle: 'Not working?',
       links: [
         {url: 'https://support.zoom.us/hc/en-us/articles/202952568-My-Video-Camera-Isn-t-Working',
-          linkName: 'My Video/Camera is not working'
+          linkName: 'My Video/Camera is not working',
         },
         {url: 'https://support.zoom.us/hc/en-us/articles/204484835-My-Audio-is-Not-Working-on-iOS-or-Android',
-          linkName: 'My Audio is not working on iOS or Android'
+          linkName: 'My Audio is not working on iOS or Android',
         },
-      ]
+      ],
     },
   ],
 
@@ -71,7 +71,7 @@ const zoomData = {
     {linkTitle: 'Meeting Controls',
       vidLink: 'https://www.youtube.com/embed/ygZ96J_z4AY',
     },
-  ]
+  ],
 };
 
 const rendered = Mustache.render(tutorialTemplate, zoomData);
