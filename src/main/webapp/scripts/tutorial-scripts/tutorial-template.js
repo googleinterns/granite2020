81d7e03af79ccd7c35b60c84adef027ad4fd7e71
Niharika Bhaskar <n2bhaska@ucsd.edu>
2020-08-25 11:21:02 -0700

    Switched to backticks and removed /n

--- src/main/webapp/scripts/tutorial-scripts/tutorial-template.js
+++ src/main/webapp/scripts/tutorial-scripts/tutorial-template.js
@@ -2,34 +2,34 @@
 // Template in Javascript because the template in HTML created linter errors.
 
 const tutorialTemplate =
-'<div id="header">\n' +
-'<h1>{{toolName}}</h1>\n' +
-'<h2>{{toolDescription}}</h2>\n' +
-'<a href="{{toolLink}}" target="_blank" rel="noopener"\n' +
-'>{{toolName}} Link</a>\n' +
-'</div>\n' +
-'<div id="content">\n' +
-'<div id="content-left">\n' +
-'<h1>Useful Links</h1>\n' +
-'{{#usefulLinks}}\n' +
-'<h3>{{linkTitle}}</h3>\n'+
+'<div id="header">' +
+'<h1>{{toolName}}</h1>' +
+'<h2>{{toolDescription}}</h2>' +
+`<a href="{{toolLink}}" target="_blank" rel="noopener"
+>{{toolName}} Link</a>` +
+'</div>' +
+'<div id="content">' +
+'<div id="content-left">' +
+'<h1>Useful Links</h1>' +
+'{{#usefulLinks}}' +
+'<h3>{{linkTitle}}</h3>'+
 '{{#links}}' +
 '<li>' +
-'<a href="{{url}}" target="_blank" rel="noopener"\n' +
-'>{{linkName}}</a>' +
+`<a href="{{url}}" target="_blank" rel="noopener"
+>{{linkName}}</a>` +
 '<li>' +
 '{{/links}}' +
-'{{/usefulLinks}}\n' +
-'</div>\n' +
-'<div id="content-right">\n' +
-'<h1>Useful Videos</h1>\n' +
-'{{#usefulVids}}\n' +
-'<h2>{{linkTitle}}</h2>\n' +
-'<iframe class="tutorials-vid" src="{{vidLink}}"\n' +
-'allow="accelerometer; autoplay;\n' +
-'encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>\n' +
-'{{/usefulVids}}\n' +
-'</div>\n' +
+'{{/usefulLinks}}' +
+'</div>' +
+'<div id="content-right">' +
+'<h1>Useful Videos</h1>' +
+'{{#usefulVids}}' +
+'<h2>{{linkTitle}}</h2>' +
+`<iframe class="tutorials-vid" src="{{vidLink}}"
+allow="accelerometer; autoplay;
+encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>` +
+'{{/usefulVids}}' +
+'</div>' +
 '</div>';
 
 export {tutorialTemplate};
