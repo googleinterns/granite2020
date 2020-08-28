1a318f83dad77412780f50c03da7b903dd4caebc
Praneet <praneetmek@gmail.com>
2020-08-28 14:10:30 -0400

    lint fix

--- src/main/java/com/google/sps/servlets/UserDataServlet.java
+++ src/main/java/com/google/sps/servlets/UserDataServlet.java
@@ -32,7 +32,7 @@ public class UserDataServlet extends HttpServlet {
 
   private static final String DATASTORE_USER = "User";
 
-  private static final CLIENT_ID = "757099697912-i6jll98mfgochdo2vgjcovf64pepjesc.apps.googleusercontent.com";
+  private static final String CLIENT_ID = "757099697912-i6jll98mfgochdo2vgjcovf64pepjesc.apps.googleusercontent.com";
 
   private String userId = "";
 
