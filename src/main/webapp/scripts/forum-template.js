2c10d5593bf6c16d83e14128d6c5d54854f9a2fa
Rae Moar <rmr167@gmail.com>
2020-08-31 03:06:21 -0700

    linter style errors:

--- src/main/webapp/scripts/forum-template.js
+++ src/main/webapp/scripts/forum-template.js
@@ -5,9 +5,12 @@ const forumTemplate =
 '<div class="forum-element {{elementType}}">\n' +
   '<div class="info-element vert-line">\n' +
     '<div class="left">\n' +
-      '<p class="topic highlight-blue" style="display: {{topicDisplay}}">Topic: {{topic}}</p>\n' +
-      '<p class="accepted highlight-red" style="display: {{acceptedDisplay}}">Accepted Answer</p>\n' +
-      '<button class="accept-button button small-button" style="display: {{acceptButtonDisplay}}">Accept Answer</button>\n' +
+      '<p class="topic highlight-blue" style="display: {{topicDisplay}}">' +
+      'Topic: {{topic}}</p>\n' +
+      '<p class="accepted highlight-red" style="display: ' +
+      '{{acceptedDisplay}}">Accepted Answer</p>\n' +
+      '<button class="accept-button button small-button" style="display: ' +
+      '{{acceptButtonDisplay}}">Accept Answer</button>\n' +
     '</div>\n' +
     '<div class="right">\n' +
       '<p class="date">{{date}}</p>\n' +
@@ -21,13 +24,16 @@ const forumTemplate =
     '<p class="likes">{{likes}}</p>\n' +
     '<button class="reply-button button small-button">Reply</button>\n' +
     '<div class="response-form" style="display: none">\n' +
-      '<textarea class="text-input" name="text" rows="1" cols="30"></textarea>\n' +
+      '<textarea class="text-input" name="text" rows="1" cols="30">' +
+      '</textarea>\n' +
       '<button class="response-button button small-button">Post</button>\n' +
     '</div>\n' +
-    '<button class="expand-button button small-button" style="display: {{repliesDisplay}}">See {{numReplies}} Replies</button>\n' +
+    '<button class="expand-button button small-button" style="display: ' +
+    '{{repliesDisplay}}">See {{numReplies}} Replies</button>\n' +
   '</div>\n' +
   '<div class="replies" id="replies-{{id}}" style="display: none"></div>\n' +
-  '<button class="collapse-button button small-button" style="display: none">Hide Replies</button>\n' +
+  '<button class="collapse-button button small-button" style=' +
+  '"display: none">Hide Replies</button>\n' +
 '</div>';
 
 export {forumTemplate};
